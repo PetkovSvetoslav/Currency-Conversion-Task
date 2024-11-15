84a73321e59d2c52500c59b9288e28baac2d1c00
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2024-11-15 11:32:14 +0200

    Add files via upload
    add custom exceptions

---
+++ src/main/java/exchange/task/exception/ExternalApiException.java
@@ -0,0 +1,8 @@
+package exchange.task.exception;
+
+public class ExternalApiException extends RuntimeException {
+    public ExternalApiException(String message) {
+        super(message);
+    }
+}
+
